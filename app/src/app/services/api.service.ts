7d014d45aaafd6ed9602cd7538f8f5085a6c2117
Kacper Zmijak <KCZ@kmd.dk>
2021-05-26 19:57:07 +0200

    Implemented user api interface

---
+++ app/src/app/services/api.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import environment from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export default abstract class ApiService {
+  protected abstract root: string;
+
+  private headers: {
+      'Content-Type': 'application/json';
+  } | undefined;
+
+  protected constructor(protected http: HttpClient) { }
+
+  protected get<T>(actionUrl: string): Observable<T> {
+    return this.http.get<T>(this.getEndpointUrl(actionUrl));
+  }
+
+  protected post<T, TOut>(actionUrl: string, data: T): Observable<TOut> {
+    return this.http.post<TOut>(this.getEndpointUrl(actionUrl), data, {
+      headers: this.headers,
+    });
+  }
+
+  protected delete<T>(actionUrl: string): Observable<T> {
+    return this.http.delete<T>(this.getEndpointUrl(actionUrl));
+  }
+
+  protected put<T>(actionUrl: string, data: T): Observable<T> {
+    return this.http.put<T>(this.getEndpointUrl(actionUrl), data);
+  }
+
+  protected getEndpointUrl(actionUrl: string): string {
+    return `${environment.apiUrl}/${this.root}${actionUrl}`;
+  }
+}
