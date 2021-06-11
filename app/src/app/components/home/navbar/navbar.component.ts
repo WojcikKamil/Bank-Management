f956761b475c2f2f3b49e48bf3d540437fa5064b
Kacper Zmijak <KCZ@kmd.dk>
2021-06-11 21:19:51 +0200

    Added new logo to navbar

--- app/src/app/components/home/navbar/navbar.component.ts
+++ app/src/app/components/home/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import User from 'src/app/models/user';
 import UserService from 'src/app/services/user.service';
 import SettingsDialog from '../../dialogs/settings/settings.dialog';
@@ -13,7 +15,13 @@ export default class NavbarComponent {
   constructor(
       private userService: UserService,
       private dialog: MatDialog,
-  ) {}
+      private iconRegistry: MatIconRegistry, sanitizer: DomSanitizer,
+  ) {
+    iconRegistry.addSvgIcon(
+      'logo',
+      sanitizer.bypassSecurityTrustResourceUrl('../../../../assets/svg/logo.svg'),
+    );
+  }
 
   get isLoggedIn(): boolean {
     return this.userService.isUserLogged();
